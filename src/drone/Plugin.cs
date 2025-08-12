eceac55a57e002e169a790ec9d8157461d079731
SebastianYang4249 <haiyang4249@gmail.com>
2025-08-13 07:17:16 +0800

    add camera control

--- src/drone/Plugin.cs
+++ src/drone/Plugin.cs
@@ -1,35 +1,71 @@
 ﻿using BepInEx;
 using BepInEx.Logging;
+using UnityEngine;
 
-namespace drone;
-
-// Here are some basic resources on code style and naming conventions to help
-// you in your first CSharp plugin!
-// https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/coding-conventions
-// https://learn.microsoft.com/en-us/dotnet/csharp/fundamentals/coding-style/identifier-names
-// https://learn.microsoft.com/en-us/dotnet/standard/design-guidelines/names-of-namespaces
-
-// This BepInAutoPlugin attribute comes from the Hamunii.BepInEx.AutoPlugin
-// NuGet package, and it will generate the BepInPlugin attribute for you!
-// For more info, see https://github.com/Hamunii/BepInEx.AutoPlugin
-[BepInAutoPlugin]
+[BepInPlugin("com.SebastianYang.DronePlugin", "Drone Plugin", "1.0.0")]
 public partial class Plugin : BaseUnityPlugin
 {
     internal static ManualLogSource Log { get; private set; } = null!;
 
-    private void Awake()
-    {
-        // BepInEx gives us a logger which we can use to log information.
-        // See https://lethal.wiki/dev/fundamentals/logging
+    private GameObject? droneObject;
+    private bool isDroneActive = false;
+
+    private void Awake() {
         Log = Logger;
 
-        // BepInEx also gives us a config file for easy configuration.
-        // See https://lethal.wiki/dev/intermediate/custom-configs
+        Log.LogInfo($"Plugin [Drone Plugin] is loaded!");
+    }
 
-        // We can apply our hooks here.
-        // See https://lethal.wiki/dev/fundamentals/patching-code
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            Log.LogInfo("[Drone Plugin]    T key pressed, toggling drone mode.");
+            ToggleDrone();
+        }
+    }
 
-        // Log our awake here so we can see it in LogOutput.log file
-        Log.LogInfo($"Plugin {Name} is loaded!");
+    void ToggleDrone() {
+        isDroneActive = !isDroneActive;
+        Log.LogInfo($"[Drone Plugin]    Drone mode is now {(isDroneActive ? "active" : "inactive")}.");
+
+        if (isDroneActive) {
+            if (Character.localCharacter == null) {
+                Log.LogError("[Drone Plugin]    No local character found, cannot activate drone mode.");
+                return;
+            }
+
+            if (MainCamera.instance == null) {
+                Log.LogError("[Drone Plugin]    No main camera found, cannot activate drone mode.");
+                return;
+            }
+
+            Log.LogInfo("[Drone Plugin]    Pass all cheaks, activating drone mode...");
+
+            Log.LogInfo("[Drone Plugin]    Creating drone camera object...");
+            droneObject = new GameObject("DroneCamera");
+            droneObject.transform.position = Character.localCharacter.transform.position + Vector3.up * 2f;
+
+            Log.LogInfo("[Drone Plugin]    Adding DroneCameraOverride component...");
+            var droneController = droneObject.AddComponent<CameraOverride>();
+
+            Log.LogInfo("[Drone Plugin]    Setting camera override...");
+            droneController.DoOverride();
+        } else {
+            Log.LogInfo("[Drone Plugin]    Deactivating drone mode...");
+
+            if (MainCamera.instance == null) {
+                Log.LogError("[Drone Plugin]    No main camera found, cannot deactivate drone mode.");
+                return;
+            }
+
+            MainCamera.instance.SetCameraOverride(null!);
+
+            if (droneObject == null) {
+                Log.LogError("[Drone Plugin]    No drone object found, cannot deactivate drone mode.");
+                return;
+            }
+
+            Destroy(droneObject);
+        }
     }
 }
